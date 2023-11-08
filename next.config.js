9bf2679876a0992349f962c3dfa79ab1d746c3f4
RickedStudios <149860608+RickedStudios@users.noreply.github.com>
2023-11-08 11:54:00 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,30 +1,3 @@
-// const withSass = require("@zeit/next-sass");
-// const withLess = require("@zeit/next-less");
-// const withCSS = require("@zeit/next-css");
-
-// const isProd = process.env.NODE_ENV === "production";
-
-//fix: prevents error when .less files are required by node
-// if (typeof require !== "undefined") {
-//   require.extensions[".less"] = (file) => {};
-// }
-
-// module.exports = withCSS({
-//   cssModules: true,
-//   cssLoaderOptions: {
-//     importLoaders: 1,
-//     localIdentName: "[local]___[hash:base64:5]",
-//   },
-//   ...withLess(
-//     withSass({
-//       lessLoaderOptions: {
-//         javascriptEnabled: true,
-//       },
-//     })
-//   ),
-// });
-
-
 const withPlugins = require('next-compose-plugins');
 const withLess = require('next-with-less');
 
@@ -33,6 +6,7 @@ const plugins = [
     withLess,
     {
       lessLoaderOptions: {
+        // Add any Less loader options you need here
       },
     },
   ],
